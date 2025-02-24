5bcd5e4f391f2631e65c9f8e4ecaeae89012b248
NNinja1255 <jack82702@gmail.com>
2025-02-24 15:51:54 -0500

    initial commit

---
+++ Assets/Scripts/NavigationManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationManager : MonoBehaviour
+{
+    public static NavigationManager instance;
+    public Room startingRoom;
+    public Room currentRoom;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
